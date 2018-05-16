e3027bc6fcf2cda22d12fc210b433ab1fecd6ef3
biswa-pradhan <37744426+biswa-pradhan@users.noreply.github.com>
2018-05-16 23:59:35 +0530

    Register.php

--- Register.php
+++ Register.php
@@ -1,13 +1,13 @@
 <?php
-    $con = mysqli_connect("mysql10.000webhost.com", "a3288368_user", "abcd1234", "a3288368_data");
+    $con = mysqli_connect("localhost", "id5746023_123456789", "sarata2015", "id5746023_biswajit");
     
-    $name = $_POST["name"];
-    $age = $_POST["age"];
-    $username = $_POST["username"];
-    $password = $_POST["password"];
+    $UpperName = $_POST["UpperName"];
+    $LowerName = $_POST["LowerName"];
+    $EmailID = $_POST["EmailID"];
+    $Password = $_POST["Password"];
 
-    $statement = mysqli_prepare($con, "INSERT INTO user (name, username, age, password) VALUES (?, ?, ?, ?)");
-    mysqli_stmt_bind_param($statement, "siss", $name, $username, $age, $password);
+    $statement = mysqli_prepare($con, "INSERT INTO user (UpperName, LowerName, EmailID, Password) VALUES (?, ?, ?, ?)");
+    mysqli_stmt_bind_param($statement, "siss", $UpperName, $LowerName, $EmailID, $Password);
     mysqli_stmt_execute($statement);
     
     $response = array();
